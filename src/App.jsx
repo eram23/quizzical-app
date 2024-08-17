91d1908fa4df31ecf2945e61aa38b9f468a5a2fa
Erik <erikramirezatl@gmail.com>
2024-08-17 15:01:40 -0400

    updated logic and fixed bugs and errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import StartQuiz from './StartQuiz'
-import Questions from './Questions'
-import Answers from './Answers'
+import StartQuiz from './components/StartQuiz'
+import Questions from './components/Questions'
+import Answers from './components/Answers.jsx'
 import { nanoid } from 'nanoid'
 
 function App(props) {
   const [startQuiz, setStartQuiz] = useState(false) 
   const [quizData, setQuizData] = useState([])
-  // const [score, setScore] = useState(0) 
   let score = 0
 
   const [results, setResults] = useState(false)
@@ -19,22 +18,28 @@ function App(props) {
     3: ''
   })
   
-  // this useEffect gets the data from the API and stores in the quizzicalData state
-  useEffect(() => {
+  // fetches quiz data only when called
+  function fetchQuizData() {
     fetch("https://opentdb.com/api.php?amount=5&category=31&difficulty=easy&type=multiple")
-      .then(res => res.json())
-      .then(data => setQuizData(data.results.map(quizObj => {
-        const shuffledAnswers = shuffleAnswers(quizObj.correct_answer, quizObj.incorrect_answers);
+    .then(res => res.json())
+    .then(data => setQuizData(data.results.map(quizObj => {
+      const shuffledAnswers = shuffleAnswers(quizObj.correct_answer, quizObj.incorrect_answers);
 
-        return {
-          id: nanoid(),
-          question: quizObj.question,
-          correct_answer: quizObj.correct_answer,
-          incorrect_answers: [...quizObj.incorrect_answers],
-          answers: shuffledAnswers,
-          selected_answer: ""
-        }
-      })))
+      return {
+        id: nanoid(),
+        question: quizObj.question,
+        correct_answer: quizObj.correct_answer,
+        incorrect_answers: [...quizObj.incorrect_answers],
+        answers: shuffledAnswers,
+        selected_answer: "",
+        quizStarted: false
+      }
+    })))
+  }
+
+  // calls the fetchQuizData to initiate the data
+  useEffect(() => {
+    fetchQuizData()
   }, [])
 
 
@@ -42,6 +47,7 @@ function App(props) {
   // this function is used to toggle whether the quiz has started or not
   function toggleStartQuiz() {
     setStartQuiz(prevValue => !prevValue)
+    
   }
 
 
@@ -57,6 +63,30 @@ function App(props) {
     return(allAnswers)
   }
 
+  // render quiz questions
+  function getQuiz() {
+    const quiz = quizData.map((quizObj, index) => {
+
+      return (
+        <>
+          <Questions
+          question={quizObj.question}
+          />
+          <Answers
+          key={quizObj.id}
+          answers={quizObj.answers}
+          trackAnswer={(e) => trackAnswer(index, e.target.innerText)}
+          />
+          <br />
+          <br />
+        </>
+      )
+    })
+
+    return quiz
+  }
+
+  // updates state of selected answers
   function trackAnswer(index, answer) {
     let btnClicked = answer
     
@@ -71,6 +101,7 @@ function App(props) {
     
   }
 
+  // checks what answers are correct and incorrect
   function checkAnswers(num) {
     quizData.map((quizItem, index) => {
       if(quizItem.correct_answer === userAnswers[index]) {
@@ -82,36 +113,32 @@ function App(props) {
     return num
   }
 
+
   function onSubmit(event) {
     event.preventDefault() 
     checkAnswers(score) 
     setResults(prevValue => !prevValue)
   }
 
+  // resets quiz, score, result state, and user answers
+  function tryAgain(event) {
+    event.preventDefault() 
+    setStartQuiz(prevValue => prevValue = false)
+    setResults(prevValue => prevValue = false)
+    score = 0
+    fetchQuizData()
+    getQuiz()
+  }
+
   return (
     <>
-      {/* conditionally renders the start page or the quizzical form  */}
+      {/* conditionally renders quiz  */}
       {startQuiz === true ? 
+        
         <form className='quiz-form' onSubmit={onSubmit}>
+          {getQuiz()}
           {/* map below grabs each question and puts it into the Question component as a prop */}
-          {quizData.map((quizObj, index) => {
-
-            return (
-              <>
-                <Questions
-                question={quizObj.question}
-                />
-                <Answers
-                key={quizObj.id}
-                answers={quizObj.answers}
-                trackAnswer={(e) => trackAnswer(index, e.target.innerText)}
-                />
-              </>
-            )
-          })}
-          <br />
-          <br />
-          <button onClick={onSubmit}>{results === false ? 'Check Answers' : 'Try Again'}</button>
+          <button onClick={results === false ? onSubmit : tryAgain}>{results === false ? 'Check Answers' : 'Try Again'}</button>
           {results && <p>You scored {checkAnswers(score)}/5 answers</p> }
         </form>
       :
