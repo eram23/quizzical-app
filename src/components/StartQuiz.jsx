b9d378543261e58b7a7a24c52b35e3fec1b6e3f2
Erik <erikramirezatl@gmail.com>
2024-09-03 17:55:48 -0400

    fixed bug that doesn't allow image to load

--- src/components/StartQuiz.jsx
+++ src/components/StartQuiz.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import anyaImage  from '/src/assets/anya-1.png'
 
 function StartQuiz(props) {
 
 
     return (
         <div className="start-quiz-section">
-            <img className="anya-start-quiz" src="src/assets/anya-1.png" alt="the anime character anya pointing to the righ"/>
+            <img className="anya-start-quiz" src={anyaImage} alt="the anime character anya pointing to the righ"/>
             <h1>Anime Quiz</h1>
             <p>Test your anime knowledge!</p>
             <button onClick={props.toggleStartQuiz}>Start Quiz</button>
